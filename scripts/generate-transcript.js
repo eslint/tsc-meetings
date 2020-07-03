a200c53058854350cf8e29cf12f4c518053468b7
Kai Cataldo <kai@kaicataldo.com>
2020-07-02 20:03:01 -0400

    Add CI

--- scripts/generate-transcript.js
+++ scripts/generate-transcript.js
@@ -1,43 +1,59 @@
-#!/usr/bin/env node
+"use strict";
 
 const moment = require("moment");
 const generateTranscript = require("discord-transcript-generator");
 const { TOKEN, ID, ISSUE_TITLE } = process.env;
 
+/**
+ * Extract a date string from an automatically generated TSC Meeting issue title.
+ * Issue title is formatted as "TSC meeting DD-MMMM-YYYY".
+ * https://github.com/eslint/eslint-github-bot/blob/master/src/plugins/recurring-issues/index.js#L202
+ * @param {string} issueTitle The issue title from which to extract the date string.
+ * @returns {string} The extracted date string.
+ */
 function extractDateString(issueTitle) {
-    // Issue title is formatted as "TSC meeting DD-MMMM-YYYY".
-    // https://github.com/eslint/eslint-github-bot/blob/master/src/plugins/recurring-issues/index.js#L202
-    const [, dateString = null] = /(\d\d-.+-\d\d\d\d)$/.exec(issueTitle);
+
+    const [, dateString = null] = /(\d\d-.+-\d\d\d\d)$/u.exec(issueTitle);
 
     if (!dateString) {
-      throw new Error("PR title has incorrect format.")
+        throw new Error("PR title has incorrect format.");
     }
 
     return dateString;
 }
 
-function generateDate(dateString) {
-  return moment(new Date(dateString)).format("MM/DD/YYYY");
+/**
+ * Formats a date string to MM/DD/YYYY.
+ * @param {string} dateString The date string to format.
+ * @returns {string} The formatted date string.
+ */
+function formatDate(dateString) {
+    return moment(new Date(dateString)).format("MM/DD/YYYY");
 }
 
+/**
+ * Generates the transcript file output path.
+ * @param {string} dateString The date string representing the date of the transcript.
+ * @returns {string} The path in which to output the transcript.
+ */
 function generateOutputPath(dateString) {
-  return `./notes/${moment(new Date(dateString)).format("YYYY-MM-DD")}-transcript.md`;
+    return `./notes/${moment(new Date(dateString)).format("YYYY-MM-DD")}-transcript.md`;
 }
 
 (async function() {
     try {
-        const date = generateDate(extractDateString(ISSUE_TITLE));
+        const date = formatDate(extractDateString(ISSUE_TITLE));
         const output = generateOutputPath(date);
 
         await generateTranscript({
-           token: TOKEN,
-           id: ID,
-           date,
-           output,
-           name: "ESLint TSC Meeting",
+            token: TOKEN,
+            id: ID,
+            date,
+            output,
+            name: "ESLint TSC Meeting"
         });
     } catch (e) {
         console.error(`Transcript generation failed with the following error:\n${e.message}`);
-        process.exit(1);
+        throw e;
     }
-})();
+}());
