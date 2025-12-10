d1b82171e094c0b7d2a7d6b6d182ff893d03d3c5
Daniel Martens <danielmartens@protonmail.ch>
2025-12-10 17:13:42 +0100

    chore: ignore discord-transcript-generator for ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -10,5 +10,7 @@ module.exports = defineConfig([
         rules: {
             "no-console": "off",
         }
+    }, {
+        ignores: ["scripts/discord-transcript-generator.js"] // Adopted file
     }
 ]);
